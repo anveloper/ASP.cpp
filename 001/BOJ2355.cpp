88b026d2ff7c95b651d13019c1624d494450ba11
anveloper <hitedin@gmail.com>
2024-07-20 07:21:34 +0900

    sol: BOJ2355

---
+++ 001/BOJ2355.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+
+  long long a, b;
+  scanf("%lld %lld", &a, &b);
+
+  if (a > b)
+  {
+    swap(a, b);
+  }
+  printf("%lld", (b - a + 1) * (b + a) / 2);
+
+  return 0;
+}
