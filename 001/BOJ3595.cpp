d280e30962ff2267d02eb45cfde446f39358cfb0
anveloper <hitedin@gmail.com>
2024-08-01 06:34:13 +0900

    sol: BOJ3595

---
+++ 001/BOJ3595.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+inline int calc(int a, int b, int c)
+{
+  return 2 * (a * b + b * c + c * a);
+}
+
+int main(void)
+{
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int n;
+  cin >> n;
+
+  vector<int> ans(3, 1e4);
+  for (int i = 1; i * i * i <= n; i++)
+  {
+    for (int j = i; i * j * j <= n; j++)
+    {
+      if (n % (i * j))
+        continue;
+      int k = n / (i * j);
+      if (calc(i, j, k) < calc(ans[0], ans[1], ans[2]))
+      {
+        ans[0] = i;
+        ans[1] = j;
+        ans[2] = k;
+      }
+    }
+  }
+  cout << ans[0] << " " << ans[1] << " " << ans[2];
+  return 0;
+}
