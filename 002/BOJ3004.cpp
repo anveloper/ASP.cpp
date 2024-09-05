a2e339a2d3224909358c0ab0bcd3bce2df3fcfd5
anveloper <hitedin@gmail.com>
2024-09-06 06:46:33 +0900

    sol: BOJ3004

---
+++ 002/BOJ3004.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int n, total = 0;
+  cin >> n;
+  total = 2;
+  for (int i = 2; i <= n; i++)
+  {
+    total += 1 + (i / 2);
+  }
+  cout << total;
+}
