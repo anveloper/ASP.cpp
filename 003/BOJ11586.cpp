a84c98c6fddb4c769849291c4da364e964c7d2f9
anveloper <hitedin@gmail.com>
2024-10-17 21:33:19 +0900

    sol: BOJ11586

---
+++ 003/BOJ11586.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+
+  int test;
+  cin >> test;
+
+  string str;
+
+  vector<string> vec;
+
+  for (int i = 0; i < test; i++)
+  {
+    cin >> str;
+    vec.push_back(str);
+  }
+
+  int k;
+  cin >> k;
+  if (k == 1)
+  {
+    for (int i = 0; i < test; i++)
+    {
+      cout << vec[i] << "\n";
+    }
+  }
+
+  else if (k == 2)
+  {
+    for (int i = 0; i < test; i++)
+    {
+      reverse(vec[i].begin(), vec[i].end());
+      cout << vec[i] << "\n";
+    }
+  }
+
+  else
+  {
+    for (int i = test - 1; i >= 0; i--)
+    {
+      cout << vec[i] << "\n";
+    }
+  }
+
+  return 0;
+}
